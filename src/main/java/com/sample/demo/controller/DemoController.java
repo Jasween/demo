e3b0e28d0bbfb6245e1de6d7bbc83b83c730bb3f
Jasween Singh <jasweens@gmail.com>
2021-06-29 22:56:15 +1000

    Initial draft

---
+++ src/main/java/com/sample/demo/controller/DemoController.java
@@ -0,0 +1,13 @@
+package com.sample.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping(path = "/hello")
+    public String getData(){
+        return "My first application";
+    }
+}
